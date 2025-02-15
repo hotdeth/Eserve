59eca2a6c9e9b72a6de907c3620ea683cd75e3c3
hotdeth <radhiox4@icloud.com>
2025-02-15 21:12:02 +0300

    php-fpm

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,38 @@
-#this file to put the requirements for easy install 
-echo "I'm install file"
+#!/bin/bash
+
+
+
+release_file = /etc/os-release
+
+
+if grep -q "fedora" $release_file
+then
+  sudo dnf install php-fpm
+  sudo systemctl  enable php-fpm
+  sudo systemctl  start php-fpm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
