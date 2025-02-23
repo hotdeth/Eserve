1bf799022e8e4f5afcfd55923af36c88535572eb
Eredin <157521205+RidhaAlasadi@users.noreply.github.com>
2025-02-23 20:00:14 +0300

    gitting pull

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html><html>
+<head>
+    <meta charset="UTF-8">
+    <title>Eserve</title>
+    <meta name="Discription" content="A service website serve servers">
+    <meta name="author" content="Eredin">
+    <style>
+        body {
+            background-image: url(plugins/home.jpg);
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+        <img id="img1" src="plugins/Group1.png">
+        <h2 id="Eserve">Eserve</h2>
+
+        <p id="p3">your best open source destination,
+            choose your service and get
+            started with your easiest configuration setup.</p>
+
+        
+
+        <a href="{{ route('dhcp') }}" class="rectangle" id="rect1">DHCP</a>
+        <a href="ftp.html" class="rectangle" id="rect2">FTP</a>
+        <a href="dns.html" class="rectangle" id="rect3">DNS</a>
+        <a href="http.html" class="rectangle" id="rect4">HTTP</a>
+
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
