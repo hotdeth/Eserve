e924e6502553ced346fe84aacec25c36ad3fb6ab
Eredin <157521205+RidhaAlasadi@users.noreply.github.com>
2025-02-23 13:37:14 +0300

    added files for web develop

---
+++ laravel/resources/views/dhcp.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html><html>
+<head>
+
+    <meta charset="UTF-8">
+    <title>Eserve</title>
+    <meta name="Discription" content="A service website serve servers">
+    <meta name="author" content="Eredin">
+
+
+
+</head>
+<body>
+    <h1>Hello</h1>
+</body>
+
+
+
+
+</html>
