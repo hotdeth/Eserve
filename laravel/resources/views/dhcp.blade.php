fb10e1c051f4b3d8bc1b19448f060e9ff34ad4fa
hotdeth <radhiox4@icloud.com>
2025-03-01 16:14:41 +0300

    link

--- laravel/resources/views/dhcp.blade.php
+++ laravel/resources/views/dhcp.blade.php
@@ -8,29 +8,22 @@
   <link rel="stylesheet" href="css/dhcp.css">
   <style>
     body{
-<<<<<<< HEAD
             background-image: url('plugins/home.jpg');
-=======
-            background-image: url('home.png');
->>>>>>> b0640ebf3a66442deb93effaaefb09cd08b5feb7
             background-size: cover;
             background-attachment: fixed;
         }
     </style>
 </head>
 <body>
-<<<<<<< HEAD
   
   <div class="name-server-saved-button">
     <h2>DHCP Server Dashboard</h2>
     <button type="submit">Save Changes</button>
   </div>
 
-  <a href="/">
   <button class="back-button">
     <span class="arrow"></span>
   </button>
-</a>
   <form action="">
     
     <div class="header-form">
@@ -40,37 +33,10 @@
     <div class="domin-name">
       <label for="name">Domin name</label>
       <input type="text" required>
-=======
-
-  <button class="back-button">
-    <span class="arrow"></span>
-  </button>
-  <form action="">
-
-
-    <h2 class="server-name">DHCP Configuration System</h2>
-    
-    <div class="domin-name">
-      <input type="text" required=" ">
-      <label for="name">
-        <span style="transition-delay: 0ms;">D</span>
-        <span style="transition-delay: 50ms;">o</span>
-        <span style="transition-delay: 100ms;">m</span>
-        <span style="transition-delay: 150ms;">i</span>
-        <span style="transition-delay: 200ms;">n</span>
-        <span style="transition-delay: 250ms;"> </span>
-        <span style="transition-delay: 300ms;">N</span>
-        <span style="transition-delay: 350ms;">a</span>
-        <span style="transition-delay: 400ms;">m</span>
-        <span style="transition-delay: 450ms;">e</span>
-        <span style="transition-delay: 500ms;">:</span>
-      </label>
->>>>>>> b0640ebf3a66442deb93effaaefb09cd08b5feb7
     </div>
 
   <div class="network-subnet">
     <div class="network">
-<<<<<<< HEAD
       <label for="name">
         Netmask
       </label>
@@ -100,71 +66,10 @@
         Range End
       </label>
       <input type="text" required=" ">
-=======
-      <input type="text" required=" ">
-      <label>
-        <span style="transition-delay: 0ms;">N</span>
-        <span style="transition-delay: 50ms;">e</span>
-        <span style="transition-delay: 100ms;">t</span>
-        <span style="transition-delay: 150ms;">w</span>
-        <span style="transition-delay: 200ms;">o</span>
-        <span style="transition-delay: 250ms;">r</span>
-        <span style="transition-delay: 300ms;">k</span>
-        <span style="transition-delay: 350ms;"> </span>
-        <span style="transition-delay: 400ms;">I</span>
-        <span style="transition-delay: 450ms;">D</span>
-        <span style="transition-delay: 550ms;">:</span>
-      </label>
-    </div>
-
-    <div class="subnet">
-      <input type="text" required=" ">
-      <label>
-        <span style="transition-delay: 0ms;">S</span>
-        <span style="transition-delay: 50ms;">u</span>
-        <span style="transition-delay: 100ms;">b</span>
-        <span style="transition-delay: 150ms;">n</span>
-        <span style="transition-delay: 200ms;">e</span>
-        <span style="transition-delay: 250ms;">t</span>
-        <span style="transition-delay: 300ms;">:</span>
-      </label>
-    </div>
-  </div>
-
-
-  <div class="s-e-range">
-    <div class="srange">
-      <input type="text" required=" ">
-      <label>
-        <span style="transition-delay: 0ms;">S</span>
-        <span style="transition-delay: 50ms;"> </span>
-        <span style="transition-delay: 100ms;">R</span>
-        <span style="transition-delay: 150ms;">a</span>
-        <span style="transition-delay: 200ms;">n</span>
-        <span style="transition-delay: 250ms;">g</span>
-        <span style="transition-delay: 300ms;">e</span>
-        <span style="transition-delay: 350ms;">:</span>
-      </label>
-    </div>
-
-    <div class="erange">
-      <input type="text" required=" ">
-      <label>
-        <span style="transition-delay: 0ms;">E</span>
-        <span style="transition-delay: 50ms;"> </span>
-        <span style="transition-delay: 100ms;">R</span>
-        <span style="transition-delay: 150ms;">a</span>
-        <span style="transition-delay: 200ms;">n</span>
-        <span style="transition-delay: 250ms;">g</span>
-        <span style="transition-delay: 300ms;">e</span>
-        <span style="transition-delay: 350ms;">:</span>
-      </label>
->>>>>>> b0640ebf3a66442deb93effaaefb09cd08b5feb7
     </div>
 
   </div>
 
-<<<<<<< HEAD
   
   
   <div class="def-router-op-subnetmask">
@@ -194,83 +99,10 @@
         
       <div class="inp2">
         <input type="text" required>
-=======
-
-
-<div class="def-router-op-subnetmask">
-
-  <div class="def-router">
-    <input type="text" required=" ">
-        <label>
-          <span style="transition-delay: 0ms;">D</span>
-          <span style="transition-delay: 50ms;">e</span>
-          <span style="transition-delay: 100ms;">f</span>
-          <span style="transition-delay: 150ms;">a</span>
-          <span style="transition-delay: 200ms;">u</span>
-          <span style="transition-delay: 250ms;">l</span>
-          <span style="transition-delay: 300ms;">t</span>
-          <span style="transition-delay: 350ms;"> </span>
-            <span style="transition-delay: 400ms;">R</span>
-            <span style="transition-delay: 450ms;">o</span>
-            <span style="transition-delay: 500ms;">u</span>
-            <span style="transition-delay: 550ms;">t</span>
-            <span style="transition-delay: 600ms;">e</span>
-            <span style="transition-delay: 650ms;">r</span>
-            <span style="transition-delay: 700ms;">:</span>
-          </label>
-        </div>
-        <div class="op-subnetmask">
-          <input type="text" required=" ">
-          <label>
-            <span style="transition-delay: 0ms;">O</span>
-            <span style="transition-delay: 50ms;">p</span>
-            <span style="transition-delay: 100ms;">t</span>
-            <span style="transition-delay: 150ms;">t</span>
-            <span style="transition-delay: 200ms;">i</span>
-            <span style="transition-delay: 250ms;">o</span>
-            <span style="transition-delay: 300ms;">n</span>
-            <span style="transition-delay: 350ms;"> </span>
-            <span style="transition-delay: 400ms;">S</span>
-            <span style="transition-delay: 450ms;">u</span>
-            <span style="transition-delay: 500ms;">b</span>
-            <span style="transition-delay: 550ms;">n</span>
-            <span style="transition-delay: 600ms;">e</span>
-            <span style="transition-delay: 650ms;">t</span>
-            <span style="transition-delay: 700ms;"> </span>
-            <span style="transition-delay: 750ms;">M</span>
-            <span style="transition-delay: 800ms;">a</span>
-            <span style="transition-delay: 850ms;">s</span>
-            <span style="transition-delay: 900ms;">k</span>
-            <span style="transition-delay: 950ms;">:</span>
-          </label>
-    </div>
-    
-  </div>
-
-    <div class="DNS">
-      <div>
-        <label>
-          <span style="transition-delay: 0ms;">D</span>
-          <span style="transition-delay: 50ms;">N</span>
-          <span style="transition-delay: 100ms;">S</span>
-          <span style="transition-delay: 150ms;">:</span>
-        </label>
-      </div>
-
-      <div class="inputs">
-        <div class="inp1" >
-          <input type="text" required=" ">
-        </div>
-        
-        <div class="inp2">
-          <input type="text" required=" ">
-        </div>
->>>>>>> b0640ebf3a66442deb93effaaefb09cd08b5feb7
       </div>
       
     </div>
 
-<<<<<<< HEAD
   <div class="leasetime">
 
     <label>Lease Time</label>
@@ -280,32 +112,6 @@
   
 
 </form>
-=======
-<div class="lease-button">
-    <div class="leasetime">
-
-        <input type="text" required=" ">
-        <label>
-            <span style="transition-delay: 0ms;">L</span>
-            <span style="transition-delay: 50ms;">e</span>
-            <span style="transition-delay: 100ms;">a</span>
-            <span style="transition-delay: 150ms;">s</span>
-            <span style="transition-delay: 200ms;">e</span>
-            <span style="transition-delay: 250ms;"> </span>
-            <span style="transition-delay: 300ms;">T</span>
-            <span style="transition-delay: 350ms;">i</span>
-            <span style="transition-delay: 400ms;">m</span>
-            <span style="transition-delay: 450ms;">e</span>
-            <span style="transition-delay: 500ms;">:</span>
-        </label>
-    </div>
-    <div class="save-button">
-      <button type="submit">Save</button>
-    </div>
-    </div>
-
-  </form>
->>>>>>> b0640ebf3a66442deb93effaaefb09cd08b5feb7
 
 
 
