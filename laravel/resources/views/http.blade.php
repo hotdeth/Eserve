e9bbdd4e8669ef5fe0d27f82adcb020b9c2e8b29
Eredin <157521205+RidhaAlasadi@users.noreply.github.com>
2025-03-05 02:34:10 +0300

    Add HTTP page

--- laravel/resources/views/http.blade.php
+++ laravel/resources/views/http.blade.php
@@ -3,57 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTTP Configuration Settings</title>
+    <title>HTTP Configuration</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/http.css">
-
 </head>
 <body>
+    <div class="container">
     <a href="/">
-        <button class="back-btn">
+        <button class="back-button">
         <span class="arrow"></span>
         </button>
     </a>
-    
-    <div class="settings-box">
-        <h2>HTTP Configuration Settings</h2>
-        
-        <div class="form-row">
-            <div class="form-group">
-                <label for="port">Port (80 - 1600)</label>
-                <input type="number" id="port" min="80" max="1600" oninput="validatePort(this)">
+        <div class="settings-card">
+            <h2>HTTP Configuration Settings</h2>
+            <div class="input-group">
+                <div>
+                    <label>Port (80 - 1600)</label>
+                    <input type="number" min="80" max="1600" placeholder="Enter port">
+                </div>
+                <div>
+                    <label>Index</label>
+                    <input type="text" placeholder="Index file">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="index">Index</label>
-                <input type="text" id="index">
+            <div class="input-group">
+                <div>
+                    <label>Server Name</label>
+                    <input type="text" placeholder="Server name">
+                </div>
+                <div>
+                    <label>Directory (Root)</label>
+                    <input type="text" placeholder="Root directory">
+                </div>
             </div>
-        </div>
-
-        <div class="form-row">
-            <div class="form-group">
-                <label for="server">Server Name</label>
-                <input type="text" id="server">
+            <div class="buttons">
+                <button class="upload-btn"><i class="fas fa-upload"></i> Upload File</button>
+                <button class="save-btn">Save Changes</button>
             </div>
-            <div class="form-group">
-                <label for="directory">Directory (Root)</label>
-                <input type="text" id="directory">
-            </div>
-        </div>
-
-        <div class="buttons">
-            <label for="file-upload" class="upload-btn">
-            Upload File 
-            <img src="/plugins/image1.png" ></label>
-            <input type="file" id="file-upload" hidden>
-            
-            <button class="save-btn">Save Changes</button>
         </div>
     </div>
-
-    <script>
-        function validatePort(input) {
-            if (input.value < 80) input.value = 80;
-            if (input.value > 1600) input.value = 1600;
-        }
-    </script>
 </body>
 </html>
