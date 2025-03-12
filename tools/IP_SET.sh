73bfc8314b0aea8d34833fe71fd24ab3a4b1b46b
hotdeth <radhiox4@icloud.com>
2025-03-13 00:03:03 +0300

    add helper tool

---
+++ tools/IP_SET.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "config your interface ip"
+
+
+
+
+
+
+read -p "Enter your ip address:" ipaddress
+read -p "SubnetMask in slash:" subnet
+read -p "Gateway:" gateway
+read -p "DNS1" DNS1
+read -p "DNS2" DNS2
+
+
+echo " 
+network:
+ version: 2
+ renderer: NetworkManager
+ ethernets:
+   eth0:
+     dhcp4: no
+     addresses: [$ipaddress/$subnet]
+     gateway4:  $gateway 
+     nameservers:
+         addresses: [$DNS1,$DNS2] " > /etc/netplan/01-network-manager-all.yaml
