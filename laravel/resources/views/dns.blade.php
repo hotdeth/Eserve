f1c57f2974fc735b7f7c121e515741a5e76d3740
hotdeth <radhiox4@icloud.com>
2025-03-16 19:40:05 +0300

    comming soon

--- laravel/resources/views/dns.blade.php
+++ laravel/resources/views/dns.blade.php
@@ -3,13 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    
-<h1>hey, this is dns page </h1>
-<input type="text">
+<style>
 
+    h1{
+        color: white;
+        text-align: center;
+        padding: 20%;
+    }
+    p{
+        color: white;
+        text-align: center;
+    }
+   a{
+    color: red;
+   }
+</style>
+
+</head>
+<body style="background-color: black;">
+    
+<h1>Comming soon</h1>
+<p>The DNS management system</p>
 </body>
 </html>
